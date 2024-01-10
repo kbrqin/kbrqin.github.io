4f48a4205b23f24449a15f29950e4a5a5514708c
kbrqin <70354257+kbrqin@users.noreply.github.com>
2024-01-10 13:22:32 -0500

    added illust

--- src/components/leftHalf/LeftHalf.jsx
+++ src/components/leftHalf/LeftHalf.jsx
@@ -1,11 +1,15 @@
 import About from '../about/About';
 import Projects from '../projects/Projects';
+import leftpic from '../../img/leftpic.png';
 import './lefthalf.css';
 
 const LeftHalf = () => {
     return (
         <div className='lh'>
             <div className="lh-wrapper">
+                <div className="lh-pic-wrapper">
+                    <img src={leftpic} alt='self illustration' className='lh-pic'></img>
+                </div>
                 <div className="lh-nav-wrapper">
                     <div className="lh-projects">&gt; Projects</div>
                     <div className="lh-about">&gt; About</div>
