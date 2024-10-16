8b5abd3ae0ebc20b37f42616c963b258c8e98df5
Kim Guo <kbrqin@gmail.com>
2024-10-15 23:29:51 -0400

    updated whitespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
